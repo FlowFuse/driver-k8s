fe5b84a46cf91b1fba8debd5e1216ae0e56b30d8
Ben Hardill <b.hardill@gmail.com>
2023-05-09 09:27:39 +0100

    Update kubernetes.js

--- kubernetes.js
+++ kubernetes.js
@@ -326,7 +326,7 @@ const createService = async (project, options) => {
 const createIngress = async (project, options) => {
     const prefix = project.safeName.match(/^[0-9]/) ? 'srv-' : ''
 
-    console.log("K8S DRIVER: start parse ingress template");
+    console.log('K8S DRIVER: start parse ingress template')
     const localIngress = JSON.parse(JSON.stringify(ingressTemplate))
     localIngress.metadata.name = project.safeName
     localIngress.spec.rules[0].host = project.safeName + '.' + options.domain
