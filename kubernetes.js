6e77265843b621131c2006732ab3737d5f8a95b1
Ben Hardill <ben@flowforge.com>
2023-05-23 16:43:00 +0100

    Debug

--- kubernetes.js
+++ kubernetes.js
@@ -860,9 +860,12 @@ module.exports = {
         const prefix = project.safeName.match(/^[0-9]/) ? 'srv-' : ''
         if (await project.getSetting('ha')) {
             const endpoints = await this._k8sApi.readNamespacedEndpoints(`${prefix}${project.safeName}`, this._namespace)
+            this._app.log.info(JSON.stringify(endpoints))
             const addresses = endpoints.body.subsets[0].addresses.map(a => { return a.ip })
+            this._app.log.info(addresses)
             const logRequests = []
-            for (address in addresses) {
+            for (const address in addresses) {
+                this._app.log.info(address)
                 logRequests.push(got.get(`http://${address}:2880/flowforge/logs`).json())
             }
             const results = await Promise.all(logRequests)
