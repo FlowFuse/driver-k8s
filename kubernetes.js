d4a6550716a969bd1fb29b030453d0d8c6313c0c
Ben Hardill <ben@flowforge.com>
2024-05-22 13:31:11 +0100

    fix lint

--- kubernetes.js
+++ kubernetes.js
@@ -371,7 +371,6 @@ const createIngress = async (project, options) => {
 }
 
 const createCustomIngress = async (project, hostname, options) => {
-    try {
     const prefix = project.safeName.match(/^[0-9]/) ? 'srv-' : ''
     const url = new URL(project.url)
     url.host = hostname
@@ -415,9 +414,6 @@ const createCustomIngress = async (project, hostname, options) => {
     console.log(JSON.stringify(customIngress))
 
     return customIngress
-    } catch (err) {
-        console.log(err)
-    }
 }
 
 const createProject = async (project, options) => {
